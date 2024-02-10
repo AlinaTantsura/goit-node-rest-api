de34921adc6c0015d86904af65162f5931d5d782
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2024-02-10 13:07:32 +0200

    Summary

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,13 +1,58 @@
 import contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res) => {
-    res.json(await contactsService.listContacts());
+    res.json({
+        status: 'success',
+        code: 200,
+        data: await contactsService.listContacts()
+    });
 };
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    const { id } = req.params;
+    const data = await contactsService.getContactById(id);
+    if (!data) {
+        res.json({
+        status: 'No content',
+        code: 204,
+        data: "ID isn't found"
+    })
+    }
+    else {
+        res.json({
+        status: 'success',
+        code: 200,
+        data
+    })
+    }
+    
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    const { id } = req.params;
+    const data = await contactsService.removeContact(id);
+    if (!data) res.json({ status: 'No content', code: 204, data: "ID isn't found" })
+    else{ res.status(204).json() }
+    
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+    const { name, email, phone } = req.body;
+    const contact = await contactsService.addContact(name, email, phone);
+    res.status(201).json({
+        status: 'success',
+        code: 201,
+        data: contact
+    })
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res) => {
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    const contact = await contactsService.updateContact(id, {name, email, phone});
+    res.json({
+        status: 'success',
+        code: 200,
+        data: contact
+    })
+};
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -6,12 +6,15 @@ const contactsPath = join(process.cwd(),"db", 'contacts.json')
 
 async function listContacts() {
   // ...твій код. Повертає масив контактів.
-  return await readFile(contactsPath).then(data => JSON.parse(data)).catch(err => err);
+  // return await readFile(contactsPath).then(data => JSON.parse(data)).catch(err => err);
+  const list = await readFile(contactsPath);
+  return JSON.parse(list);
+
 }
 
 async function getContactById(contactId) {
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  const list = await readFile(contactsPath).then(data => JSON.parse(data)).catch(err => err);
+  const list = await listContacts();
   const index = list.findIndex(item => item.id === contactId);
   if (index === -1) return null
   else return list[index];
@@ -19,12 +22,12 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-  const list = await readFile(contactsPath).then(data => JSON.parse(data)).catch(err => err);
+  const list = await listContacts();
   const index = list.findIndex(item => item.id === contactId);
   if (index === -1) return null
   else{
     const filterList = list.filter(item => item.id !== contactId);
-    const listJSON = JSON.stringify(filterList);
+    const listJSON = JSON.stringify(filterList, null, 2);
     return await writeFile(contactsPath, listJSON).then(() => list[index]).catch(err => err);}
 }
 
@@ -32,11 +35,22 @@ async function addContact(name, email, phone) {
   // ...твій код. Повертає об'єкт доданого контакту (з id).
   const id = randomUUID();
   const newContact = { id, name, email, phone };
-  const list = await readFile(contactsPath).then(data => JSON.parse(data)).catch(err => err);
+  const list = await listContacts();
   list.push(newContact)
-  const listJSON = JSON.stringify(list);
-  return await writeFile(contactsPath, listJSON).then(() => newContact).catch(err => err);
+  const listJSON = JSON.stringify(list, null, 2);
+  await writeFile(contactsPath, listJSON).then(() => newContact).catch(err => err);
+  return newContact;
 };
 
-// export const contactsService = { listContacts, getContactById, removeContact, addContact };
-export default { listContacts, getContactById, removeContact, addContact};
+async function updateContact(id, {name, email, phone}) {
+  const list = await listContacts();
+  const index = list.findIndex(contact => contact.id === id);
+  const contactUpdated = { id, name, email, phone};
+  const newList = [...list.splice(0, index), contactUpdated, ...list.splice(index + 1,)];
+  await writeFile(contactsPath, JSON.stringify(newList, null, 2));
+ 
+  return contactUpdated;
+
+};
+
+export default { listContacts, getContactById, removeContact, addContact, updateContact};
